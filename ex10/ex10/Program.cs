8d5509e2f615ef703d83298b3d723dc25a50f6ad
slimz <145461064+slimjimz@users.noreply.github.com>
2024-03-14 10:22:26 -0400

    save

---
+++ ex10/ex10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Xml.Serialization;
+
+namespace ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            Console.WriteLine("***************");
+            Console.WriteLine("Menu: ");
+            Console.WriteLine("1 - New Game");
+            Console.WriteLine("2 - Load Game");
+            Console.WriteLine("3 - Options");
+            Console.WriteLine("4 - Quit");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("***************");
+            Console.WriteLine("");
+
+            //Promtp choices
+            Console.WriteLine("Enter your choice (1, 2, 3, 4)");
+            choice = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            while (choice > 0) 
+            
+            {
+                if (choice == 1)
+                {
+                    Console.WriteLine("Starting New Game");
+                }
+                else if (choice == 2)
+                {
+                    Console.WriteLine
+                }
+
+
+
+
+
+
+
+            } // end of while
+            
+                    
+        
+                 
+        }
+    }
+}
