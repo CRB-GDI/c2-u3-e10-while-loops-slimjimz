54a58ef42fdc834b20d11edca5221e233f98ac58
slimz <145461064+slimjimz@users.noreply.github.com>
2024-03-20 09:03:44 -0400

    done

--- ex10/ex10/Program.cs
+++ ex10/ex10/Program.cs
@@ -22,24 +22,48 @@ namespace ex10
             choice = int.Parse(Console.ReadLine());
             Console.WriteLine();
 
-            while (choice > 0) 
-            
+            while (choice < 4) 
+
             {
                 if (choice == 1)
                 {
-                    Console.WriteLine("Starting New Game");
+                    Console.WriteLine("Starting New Game...");
+                    Console.WriteLine("");
                 }
                 else if (choice == 2)
                 {
-                    Console.WriteLine
+                    Console.WriteLine("Loading Save File...");
+                    Console.WriteLine("");
                 }
+                else if (choice == 3)
+                {
+                    Console.WriteLine("Loading Options...");
+                    Console.WriteLine("");
+                }
+                else if (choice == 4)
+                {
+                     
+                }
+               
+
+                Console.WriteLine("***************");
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("1 - New Game");
+                Console.WriteLine("2 - Load Game");
+                Console.WriteLine("3 - Options");
+                Console.WriteLine("4 - Quit");
+                Console.WriteLine("Hello, World!");
+                Console.WriteLine("***************");
+                Console.WriteLine("");
+
+                //Promtp choices
+                Console.WriteLine("Enter your choice (1, 2, 3, 4)");
+                choice = int.Parse(Console.ReadLine());
+                Console.WriteLine();
 
 
 
-
-
-
-
+                
             } // end of while
             
                     
